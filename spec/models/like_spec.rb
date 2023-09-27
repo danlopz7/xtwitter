f0f4911144012f371919fafb4081569528dfb301
danlopz7 <dlopez@ioogo.com>
2023-09-27 11:53:11 -0600

    I had some testing models that came from a merging, now is all clean. I refactor Like test and created Follow Spec test now running successfully

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:tweet) }
+  end
+  
+  describe 'uniqueness validation' do
+    subject { FactoryBot.create(:like) }
+    it { should validate_uniqueness_of(:tweet_id).scoped_to(:user_id) }
+  end
 end
