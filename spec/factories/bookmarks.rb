817170e570a86f46ef892ee84e22cd878d95229b
danlopz7 <dlopez@ioogo.com>
2023-09-27 11:14:57 -0600

    Bookmark tests created and running successfully

--- spec/factories/bookmarks.rb
+++ spec/factories/bookmarks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :bookmark do
-    user { association :user }
-    tweet { association :tweet }
+    user
+    tweet
   end
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  
+
   describe 'association' do
     it { should belong_to(:user) }
     it { should belong_to(:tweet) }
@@ -10,5 +10,23 @@ RSpec.describe Bookmark, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:tweet_id) }
+  end
+
+  # Scope tests
+  describe ".user_bookmarks" do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:tweet) { create(:tweet) }
+
+    before do
+      # Create three bookmarks for user1 and one for user2
+      create_list(:bookmark, 3, user: user1, tweet: tweet)
+      create(:bookmark, user: user2, tweet: tweet)
+    end
+
+    it "returns bookmarks belonging to a given user" do
+      expect(Bookmark.user_bookmarks(user1.id).count).to eq(3)
+      expect(Bookmark.user_bookmarks(user2.id).count).to eq(1)
+    end
   end
 end
