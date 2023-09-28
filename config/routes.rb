d01ad0b6fc6e24a5358bb69d9fad6e85fe9313fb
danlopz7 <dlopez@ioogo.com>
2023-09-28 11:22:39 -0600

    refactored again, I'm about to start tests gonna read documentation right now

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,8 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [] do
-    resources :tweets, only: [:index] to: 'tweets#index' do
-      collection do 
-        get 'page/:page', action: :index
-      end
+    resources :tweets, only: [], to: 'tweets#index' do
+      get 'page/:page', action: :index, on: :collection
     end
     get 'tweets_and_replies/page/:page', on: :member, to: 'tweets#tweets_and_replies'
   end
