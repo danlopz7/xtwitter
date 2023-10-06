ec444d6bcf861b16dcfefa86ce9992da85a21bab
danlopz7 <dlopez@ioogo.com>
2023-10-06 11:54:22 -0600

    created web namespace and created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -30,33 +30,35 @@ Rails.application.routes.draw do
     end
   end
 
-  # Ruta del perfil del usuario actual
-  get 'profile', to: 'users#profile', as: 'current_user_profile'
+  namespace :web do
+    # Ruta del perfil del usuario actual
+    get 'profile', to: 'users#profile', as: 'current_user_profile'
   
-  # Rutas relacionadas con usuarios específicos
-  # GET /user/:username Should display the personal information of the user: 
-  resources :user, only: ["show"], param: :username, controller: "users" do
-    #get 'user/:username', to: 'users#show'
-    member do
-      # estas rutas mostraran los tweets y replies de un usuario especifico
-      get 'tweets(/page/:page)', to: 'tweets#index', as: 'tweets'
-      get 'tweets_and_replies(/page/:page)', to: 'tweets#tweets_and_replies', as: 'tweets_and_replies'
+    # Rutas relacionadas con usuarios específicos
+    # GET /user/:username Should display the personal information of the user: 
+    resources :user, only: ["show"], param: :username, controller: "users" do
+      #get 'user/:username', to: 'users#show'
+      member do
+        # estas rutas mostraran los tweets y replies de un usuario especifico
+        get 'tweets(/page/:page)', to: 'tweets#index', as: 'tweets'
+        get 'tweets_and_replies(/page/:page)', to: 'tweets#tweets_and_replies', as: 'tweets_and_replies'
+      end
     end
-  end
 
-  # Rutas relacionadas con tweets del usuario actual y los que sigue
-  # Get 'tweets', to: 'tweets#index_for_current_user'  # TweetsController#Index 
-  # GET /tweets => tweets#index mostrara los tweets del usuario junto con los tweets de los usuarios que sigue. se redirige aqui al loguearse o registrarse using redirect_to
-  # rutas en las que un current user puede interactuar con un determinado tweet
-  resources :tweets, only: [ :index, :show, :create, :update, :new ] do
-    member do
-      post 'retweet'
-      post 'quote'
-      post 'like'
-      delete 'unlike'
-      post 'bookmark'
-      get 'stats'
-      post 'replies', to: 'tweets#create_reply'
+    # Rutas relacionadas con tweets del usuario actual y los que sigue
+    # Get 'tweets', to: 'tweets#index_for_current_user'  # TweetsController#Index 
+    # GET /tweets => tweets#index mostrara los tweets del usuario junto con los tweets de los usuarios que sigue. se redirige aqui al loguearse o registrarse using redirect_to
+    # rutas en las que un current user puede interactuar con un determinado tweet
+    resources :tweets, only: [ :index, :show, :create, :update, :new ] do
+      member do
+        post 'retweet'
+        post 'quote'
+        post 'like'
+        delete 'unlike'
+        post 'bookmark'
+        get 'stats'
+        post 'replies', to: 'tweets#create_reply'
+      end
     end
   end
 end
