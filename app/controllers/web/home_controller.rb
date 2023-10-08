39ed13ea996416e6a5f326696903e0bc38d2abae
Daniel <daen.lopez14@gmail.com>
2023-10-07 23:51:05 -0600

    changed to return 10 randoms tweets no matter any order

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -7,7 +7,7 @@ class Web::HomeController < ApplicationController
       redirect_to web_tweets_path
     else
       # Traer los primeros 10 tweets 
-      @random_tweets = Tweet.order("RANDOM()").limit(10).sort_by(&:created_at).reverse
+      @random_tweets = Tweet.order("RANDOM()").limit(10)
       @tweets_with_stats = @random_tweets.map do |tweet|
         {
           tweet: tweet,
